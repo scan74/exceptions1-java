85e68b41cda1d4b5c0345e6ca70a5fbc05821abd
Sergio Camara <scamara74@hotmail.com>
2019-02-01 16:01:24 -0300

    solução ruim

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,9 +7,11 @@ import java.util.Scanner;
 
 import model.entities.Reservation;
 
-public class Program {
+public class Program 
+{
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) throws ParseException 
+	{
 		
 		Scanner sc = new Scanner(System.in);
 		
@@ -22,10 +24,12 @@ public class Program {
 		System.out.print("Check-out date (dd/mm/yyyy): ");
 		Date checkOut = sdf.parse(sc.next());
 		
-		if(!checkOut.after(checkIn)) {
+		if(!checkOut.after(checkIn)) 
+		{
 			System.out.println("Error in reservation: Check-out date must de after check-in date");
 		}
-		else {
+		else 
+		{
 			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);
 			
@@ -36,22 +40,11 @@ public class Program {
 			System.out.print("Check-out date (dd/mm/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 			
-			Date now = new Date();
-			if(checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: reserve dates for update must be future");
-			}
-			else if(!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must de after check-in date");
-			}
-			else {
-				reservation.updateDates(checkIn, checkOut);
-				System.out.println("Reservation: " + reservation);
-			}
-			
 			reservation.updateDates(checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);
-			
-			
 		}
+		
+		sc.close();
 	}
+	
 }
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -43,9 +43,22 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MICROSECONDS);
 	}
 	
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		
+		Date now = new Date();
+		if(checkIn.before(now) || checkOut.before(now)) 
+		{
+			return "reserve dates for update must be future";
+		}
+		if(!checkOut.after(checkIn)) 
+		{
+			return "Check-out date must de after check-in date";
+		}
+		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		
+		return null; //se retornar null é por que nao deu nenhum erro
 	}
 	
 	@Override
